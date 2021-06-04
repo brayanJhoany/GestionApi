8260b23581fbc74d07ab8905ec422793e28d9c6e
brayan escobar <brayan.escobar@live.com>
2021-06-03 22:59:22 -0400

    feature(PlanDeClasesFactory): factory del plan de clases

--- database/factories/PlanDeClaseFactory.php
+++ database/factories/PlanDeClaseFactory.php
@@ -2,7 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\PlanDeClases;
+use App\Models\PlanDeClase;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PlanDeClasesFactory extends Factory
@@ -12,7 +12,7 @@ class PlanDeClasesFactory extends Factory
      *
      * @var string
      */
-    protected $model = PlanDeClases::class;
+    protected $model = PlanDeClase::class;
 
     /**
      * Define the model's default state.
